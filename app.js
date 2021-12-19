db9f576f1509e0462df207205adfcf8a980928a0
Shahbaz <shahbaz_ali@outlook.in>
2021-12-19 06:23:43 +0530

    v1

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const mail = require('./services/mail');
+const Registrations = require('./models/registration');
+const app = express();
+
+const fs = require('fs');
+
+var mail_template = fs.readFileSync('./mail_templates/confirmation_email_template.html', 'utf8');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    Registrations.find({}, (err, data) => {
+        if (err) {
+            res.send("error is", err);
+        } else {
+            res.render("index", { registrations: data });
+        }
+    });
+});
+
+app.post("/delete", (req, res) => {
+    Registrations.findByIdAndDelete(req.body._id, (err, data) => {
+        if (err) {
+            res.send("error is", err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/update", (req, res) => {
+    const { _id, name, email, phoneNumber, collegeName, rollNumber, txnID, paymentStatus } = req.body;
+    console.log(req.body);
+    Registrations.findByIdAndUpdate(_id, {
+        $set: {
+            name,
+            email,
+            phoneNumber,
+            collegeName,
+            rollNumber,
+            txnID,
+            paymentStatus
+        }
+    }, (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/verifyPayment", (req, res) => {
+    const { _id, name, email } = req.body;
+    Registrations.findByIdAndUpdate(_id, {
+        $set: {
+            paymentStatus: "Verified"
+        }
+    }, (err, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            mail.sendMail(email, "Confirmation Message", "confirmed", mail_template, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {}
+            });
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/sendMail", (req, res) => {
+    const { email } = req.body;
+    mail.sendMail(email, "Confirmation Message", "confirmed", mail_template, (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+
+app.listen(8001, () => {
+    console.log('Server started on port 8001');
+});
---
+++ models/registration.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//const DB = "mongodb+srv://"+ process.env.DB_USERNAME +":"+process.env.DB_PASSWORD+"@cluster0.ymjcf.mongodb.net/BRL_Registration?retryWrites=true&w=majority"
+
+mongoose.connect(
+    "mongodb://localhost:27017/registration",
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the database");
+        }
+    }
+);
+
+const Schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+    },
+    collegeName: {
+        type: String,
+        required: true
+    },
+    rollNumber: {
+        type: Number,
+        required: true
+    },
+    txnID: {
+        type: String,
+        required: true
+    },
+    paymentStatus: {
+        type: String,
+    }
+});
+
+const Registrations = mongoose.model("Registrations", Schema);
+
+module.exports = Registrations;
