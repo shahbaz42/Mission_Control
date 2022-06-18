75f68c1846aeb52c027cbd9a147f4e23fef6df7e
Shahbaz <shahbaz_ali@outlook.in>
2022-06-18 08:05:07 +0530

    add separate middleware for ticket verification

--- middlewares/is_authenticated.js
+++ middlewares/is_authenticated.js
@@ -4,3 +4,15 @@ exports.is_authenticated = function (req, res, next) {
     }
     res.redirect("/login");
 }   
+
+exports.is_authenticated_for_ticket_checking = function (req, res, next) {
+    const DOMAIN = process.env.DOMAIN;
+    const _id = req.params.id;
+    const qr_url = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${DOMAIN}/ticket/verify/${_id}`;
+    if (req.isAuthenticated()) {
+        // add id to request object
+        req.user.ticket_id = _id;
+        return next();
+    }
+    res.render("show_ticket", {qr_url});
+} 
