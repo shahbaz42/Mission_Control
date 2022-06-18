2c4dbaaf88c16d18a87dcc97f3c8ea30f1ac5e78
Shahbaz <shahbaz_ali@outlook.in>
2022-06-18 14:55:59 +0530

    store session info on mongoDB

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const session = require("express-session");
 const passport = require("passport");
 const rateLimit = require("express-rate-limit");
 const check_login = require("./middlewares/is_authenticated");
+const MongoStore = require('connect-mongo');
+const DB_URL = process.env.DB_URL;
 
 const limiter = rateLimit({
 	windowMs: 2 * 60 * 1000, // 10 minutes
@@ -20,13 +22,16 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(
-    session({
-        secret: process.env.SESSIONS_SECRET,
-        resave: false,
-        saveUninitialized: false,
-    })
-);
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: DB_URL,
+        autoReconnect: true,
+        ttl : 1000 * 60 * 60 * 24 * 365
+    }),
+}));
 
 app.use(passport.initialize()); //initialising passport
 app.use(passport.session()); //making express use passport.sessions
